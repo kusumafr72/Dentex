81e5ff3683209e002026f653d0229e0dee63189b
kusumafr72 <87970635+kusumafr72@users.noreply.github.com>
2024-08-29 11:35:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_cfg():
     # Force model to operate within CPU, erase if CUDA compatible devices ara available
     cfg.MODEL.DEVICE = 'cpu'
     # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-    cfg.merge_from_file('version_3/Toothsegment_3_config.yml')
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
     # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
     cfg.MODEL.WEIGHTS = output
